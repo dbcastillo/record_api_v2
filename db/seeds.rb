d502332ad78213143bf4314c4fefcbfe59fdc337
Derick Castillo <derickcastillo@Dericks-MacBook-Air-2.local>
2025-10-21 16:59:09 -0500

    Add rack-cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,20 @@ Record.create!([
     artist: "Michael Jackson",
     year: 1982,
     price: 29.99,
-    cover: "https://example.com/thriller.jpg"
+    cover: "Taylor.png"
   },
   {
     album: "Kind of Blue",
     artist: "Miles Davis",
     year: 1959,
     price: 19.99,
-    cover: "https://example.com/kindofblue.jpg"
+    cover: "GreenDay.png"
   },
   {
     album: "Back to Black",
     artist: "Amy Winehouse",
     year: 2006,
     price: 24.99,
-    cover: "https://example.com/backtoblack.jpg"
+    cover: "LimpBizkit.png"
   }
 ])
